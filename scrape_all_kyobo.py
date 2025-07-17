d7be1f80190ac4eb53642e95cb930b969923250b
thzthix <seohas0428@naver.com>
2025-07-17 14:26:13 +0900

    refactor: 불필요한 스크립트 제거 및 오류 사이트 주석 처리

--- scrape_all_kyobo.py
+++ scrape_all_kyobo.py
@@ -11,10 +11,10 @@ from datetime import datetime
 # 13개 사이트 URL 리스트
 KYOBO_URLS = [
     ("교보문고", "통합PC", "https://mmbr.kyobobook.co.kr/login"),
-    ("교보문고", "eBook", "https://mmbr.kyobobook.co.kr/login?continue=https://ebook.kyobobook.co.kr/"),
-    ("교보문고", "sam정액구독", "https://mmbr.kyobobook.co.kr/login?continue=https://sam.kyobobook.co.kr/"),
-    ("교보문고", "핫트랙스몰", "https://mmbr.kyobobook.co.kr/login?continue=https://hottracks.kyobobook.co.kr/"),
-    ("교보문고", "스토리플랫폼", "https://mmbr.kyobobook.co.kr/login?continue=https://storynew.kyobobook.co.kr/"),
+    # ("교보문고", "eBook", "https://mmbr.kyobobook.co.kr/login?continue=https://ebook.kyobobook.co.kr/"),
+    # ("교보문고", "sam정액구독", "https://mmbr.kyobobook.co.kr/login?continue=https://sam.kyobobook.co.kr/"),
+    # ("교보문고", "핫트랙스몰", "https://mmbr.kyobobook.co.kr/login?continue=https://hottracks.kyobobook.co.kr/"),
+    # ("교보문고", "스토리플랫폼", "https://mmbr.kyobobook.co.kr/login?continue=https://storynew.kyobobook.co.kr/"),
     ("교보문고", "핫트랙스관리자", "https://admin.hottracks.co.kr/admin/login/form"),
     ("교보생명", "통합로그인", "https://www.kyobo.com/dgt/web/dtm/lc/tu/login"),
     ("교보생명", "금융인증서", "https://www.kyobo.com/dgt/web/dtm/lc/tu/kftcLogin"),
