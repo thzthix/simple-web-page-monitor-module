7605e980ad3829ea262568a63e2b4f8c1826aa08
thzthix <seohas0428@naver.com>
2025-07-17 14:50:57 +0900

    refactor: 불필요한 스크립트 제거, 테스트 파일 이동 및 URL 리스트 분리

---
+++ config/urls.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+KYOBO_URLS = [
+    ("교보문고", "통합PC", "https://mmbr.kyobobook.co.kr/login"),
+    # ("교보문고", "eBook", "https://mmbr.kyobobook.co.kr/login?continue=https://ebook.kyobobook.co.kr/"),
+    # ("교보문고", "sam정액구독", "https://mmbr.kyobobook.co.kr/login?continue=https://sam.kyobobook.co.kr/"),
+    # ("교보문고", "핫트랙스몰", "https://mmbr.kyobobook.co.kr/login?continue=https://hottracks.kyobobook.co.kr/"),
+    # ("교보문고", "스토리플랫폼", "https://mmbr.kyobobook.co.kr/login?continue=https://storynew.kyobobook.co.kr/"),
+    ("교보문고", "핫트랙스관리자", "https://admin.hottracks.co.kr/admin/login/form"),
+    ("교보생명", "통합로그인", "https://www.kyobo.com/dgt/web/dtm/lc/tu/login"),
+    ("교보생명", "금융인증서", "https://www.kyobo.com/dgt/web/dtm/lc/tu/kftcLogin"),
+    ("교보생명", "카카오인증서", "https://www.kyobo.com/dgt/web/dtm/lc/tu/kakaoLogin"),
+    ("교보생명", "토스인증서", "https://www.kyobo.com/dgt/web/dtm/lc/tu/tossLogin"),
+    ("교보생명", "사내SmartON", "https://sso.kyobo.com:5443/3rdParty/certLoginFormPage.jsp"),
+    ("교보라이프플래닛", "통합PC", "https://www.lifeplanet.co.kr/lpds2/common/ua/UA01001S.dev"),
+    ("교보증권", "고객포털PC", "https://www.iprovest.com/weblogic/LOginServlet")
+]
